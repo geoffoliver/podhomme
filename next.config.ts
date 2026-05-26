4ce9f2a856816e9af73f1edee1b5478f14809445
Geoff Oliver <geoff@plan8studios.com>
2026-05-26 13:33:06 -0600

    move devIndicators for playwright tests

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'standalone',
   allowedDevOrigins: ['chonk.local', 'pods.geoffoliver.me'],
+  devIndicators: {
+    position: 'bottom-right',
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**' },
